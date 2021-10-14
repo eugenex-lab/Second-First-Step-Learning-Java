1e2fdecee2b4c0be16a910d212aca4c9377c3b4e
eugenex-lab <kolapo.eugene@gmail.com>
2021-10-14 17:01:42 +0100

    a

--- src/lab/eugene/learn/HelloWorld.java
+++ src/lab/eugene/learn/HelloWorld.java
@@ -2,31 +2,87 @@ package lab.eugene.learn;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        String text = " Hello all the world";
-        int myInt = 3344;
-        short myS = 8039;
-        long myL = 8023433;
+///PRIMITIVE TYPING
 
-        double myDec1 = 6339.3333;
-        double myFloat = 639.3333;
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        String text = " Hello all the world";
+//        int myInt = 3344;
+//        short myS = 8039;
+//        long myL = 8023433;
+//
+//        double myDec1 = 6339.3333;
+//        double myFloat = 639.3333;
+//
+//        String myChar = "j";
+//        boolean myBool = false;
+//
+//        byte myByte = 123;
+//
+//        System.out.println(text);
+//        System.out.println(myInt);
+//        System.out.println(myS);
+//        System.out.println(myL);
+//
+//        System.out.println(myDec1);
+//        System.out.println(myFloat);
+//
+//        System.out.println(myChar);
+//        System.out.println(myBool);
+//
+//        System.out.println(myByte);
+//    }
+//}
 
-        String myChar = "j";
-        boolean myBool = false;
 
-        byte myByte = 123;
+//        System.out.println("Hello WOrlds");
 
-        System.out.println(text);
-        System.out.println(myInt);
-        System.out.println(myS);
-        System.out.println(myL);
+//        String text = "Hellos";
+//        STring is a class , Hello is an object and tect is a var
 
-        System.out.println(myDec1);
-        System.out.println(myFloat);
+//
+//        String blank = " ";
+//        String player  = "Abel";
+//        int age = 60;
+//
+//        System.out.println("Do you know that" + blank + player + "is" + age + " year OLd");
 
-        System.out.println(myChar);
-        System.out.println(myBool);
+    ///WHILE LOOOPSSSSSS
 
-        System.out.println(myByte);
+    //Test Case1
+
+//        Boolean loop = 6 > 3;
+//        System.out.println(loop);
+//
+//        Boolean loopz = 500 > 3;
+//        System.out.println(loopz);
+
+    ///Test Case2
+//        double valueLoop = 600 ;
+//        boolean looping = valueLoop > 9000;
+//        System.out.println(looping);
+
+
+        ////
+//        int arsenalPoint = 50;
+//        while(arsenalPoint < 60) {
+//
+//            System.out.println(" Arsenal is SHit");
+//
+//            ///NOte it never stop printing Arsnal is shit cos theyreally are shiit
+//        }
+
+        int oleUnitedPoint = 50;
+
+        while(oleUnitedPoint < 80)
+        {
+            System.out.println(" Ole is a PE Coach" + "current points are at" + oleUnitedPoint);
+            oleUnitedPoint = oleUnitedPoint + 3 ;
+            ///NOte it never stop printing Arsnal is shit cos theyreally are shiit
+        }
+
+
+
     }
 }
 
