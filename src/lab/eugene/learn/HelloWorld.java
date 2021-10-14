af63151c354ef75c3d03870e95e1f946cac7d46d
eugenex-lab <kolapo.eugene@gmail.com>
2021-10-14 15:51:53 +0100

    Initial commit

---
+++ src/lab/eugene/learn/HelloWorld.java
@@ -0,0 +1,33 @@
+package lab.eugene.learn;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String text = " Hello all the world";
+        int myInt = 3344;
+        short myS = 8039;
+        long myL = 8023433;
+
+        double myDec1 = 6339.3333;
+        double myFloat = 639.3333;
+
+        String myChar = "j";
+        boolean myBool = false;
+
+        byte myByte = 123;
+
+        System.out.println(text);
+        System.out.println(myInt);
+        System.out.println(myS);
+        System.out.println(myL);
+
+        System.out.println(myDec1);
+        System.out.println(myFloat);
+
+        System.out.println(myChar);
+        System.out.println(myBool);
+
+        System.out.println(myByte);
+    }
+}
+
+
